4c494447c18a599fc5260597ef4d12981dd8a419
Admin <andre@vitorio.net>
2017-04-12 08:50:23 +0200

    Add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -7,17 +7,15 @@ module.exports = {
 		var encodedLocation = encodeURIComponent(location);
 		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-		return axios.get(requestUrl).then(
-
-			function(res){
-				if(res.data.cod && res.data.message) {
-					throw new Error(res.data.message);
-				} else {
-					return res.data.main.temp;
-				}
-			},
-			function(res){
-				throw new Error(res.data.message);
-			})
+		return axios.get(requestUrl)
+            .then(function(response) {
+                if(response.data.cod && response.data.message) {
+                    throw new Error('Unable to fetch weather');
+                } else {
+                    return response.data.main.temp;
+                }
+            }).catch(function(error) {
+                throw new Error('Unable to fetch weather');
+            });
 	}
 }
